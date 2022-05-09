86287b9e2940a88028f8ea9266636430594f73c9
Nam <dthnam90@gmail.com>
2022-05-09 11:36:04 +0700

    Auto unit tests

--- src/test/java/com/example/demo/UtilityTests.java
+++ src/test/java/com/example/demo/UtilityTests.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UtilityTests {
 
 	@Test
-	void test1() {
+	void calcShouldReturnCorrectResultWhenAddingTwoNumbers() throws Exception {
+		int a = 0;
+		int b = 1;
 
+		assertEquals(1, Utility.calc(a, b, Operator.ADD));
 	}
 
 }
