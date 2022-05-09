b2d210748e2c577b62f50df40f836d256c25fc33
Nam <dthnam90@gmail.com>
2022-05-09 11:29:31 +0700

    Manual test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		int a = 0;
+		int b = 1;
+		System.out.printf("Added: %d%n", Utility.calc(a, b, Operator.ADD));
+		System.out.printf("Minus: %d%n", Utility.calc(a, b, Operator.MINUS));
+		System.out.printf("Multiplied: %d%n", Utility.calc(a, b, Operator.MULTIPLY));
+		System.out.printf("Divided: %d%n", Utility.calc(a, b, Operator.DIVIDE));
 	}
-
 }
